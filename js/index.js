421a32ce9fa788fa6435469313987af6d8f03609
w0xter <w0xter@localhost.localdomain>
2019-10-27 11:59:29 +0100

    Minor Changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-function scrollTo() {
-        console.log("Click");
-          /*$('html, body').animate({ scrollTop: $('#' + id).offset().top }, 'slow');
-          return false;*/
+function navbarSpy(){
+  $(document).ready(function(){
+    let topWin = $(document).scrollTop();
+    let bottomWin = topWin + $(window).innerHeight();
+    let home = $("#home");
+    let about = $("#about")
+    let contact = $("#contactTitle");
+    console.log("--------------------------------------------------------------------------")
+    console.log("Home: H=" + home.height() + " OFT=" + home.offset().top);
+    console.log("about: H=" + about.height() + " OFT=" + about.offset().top);
+    console.log("contact: H=" + contact.height() + " OFT=" + contact.offset().top);
+    console.log("TopWin: " + topWin + " BottomWin: " + bottomWin);
+    console.log("--------------------------------------------------------------------------")
+    if(topWin < about.offset().top){
+     console.log("Home")
+    }else if(topWin > about.offset().top && bottomWin < contact.offset() ){
+      console.log("About");
+    }else{
+      console.log("Contact");
     }
+   });
+}
