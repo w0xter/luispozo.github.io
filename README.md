1ee7dc8f8fa760fc1aba56fbe3e55dae7d700e3b
w0xter <luispozogilo@gmail.com>
2020-05-10 19:20:21 +0200

    Preparing merge

--- README.md
+++
@@ -1,2 +0,0 @@
-# luispozo.github.io
-github page 
---
+++ build/precache-manifest.b13d91dd7458d06f73742ba25c31b504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e42ee6b33c9fe4c708792d0b7259cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe8ffc756fd410eef8e",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "ef9d38227ddea795cf61",
+    "url": "/static/css/main.510f3c55.chunk.css"
+  },
+  {
+    "revision": "cbe8ffc756fd410eef8e",
+    "url": "/static/js/2.d985b2e1.chunk.js"
+  },
+  {
+    "revision": "58ae91058ba718793124d67c181f472a",
+    "url": "/static/js/2.d985b2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9d38227ddea795cf61",
+    "url": "/static/js/main.1e790fbd.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  }
+]);
