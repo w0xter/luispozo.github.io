ee209dcdb64a75995dfd013ed14e5d9f03a27b6c
w0xter <luispozogilo@gmail.com>
2021-01-12 13:42:26 +0100

    Minor Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
               mappings, enabling the use of existing querytranslation (SPARQL-to-SQL) techniques and tools. [...]
             </Paragraph>
 
-            <Button type="primary" className="shadow">Continue Reading</Button>
+            <Button href="https://www.researchgate.net/profile/David_Chaves-Fraga/publication/345602766_Morph-CSV_Virtual_Knowledge_Graph_Access_for_Tabular_Data/links/5fa919a6458515157bf732fc/Morph-CSV-Virtual-Knowledge-Graph-Access-for-Tabular-Data.pdf" type="primary" className="shadow">Continue Reading</Button>
             </div>
           </div>
         </Col>
@@ -91,7 +91,7 @@ const IndexPage = () => (
             <div className="p-2">
             <Row gutter={[32,32]} justify="space-between" align="middle">
               <Col>
-                <Title level={4}>Morph-CSV: Virtual Knowledge Graph Access for Tabular Data</Title>
+                <Title level={4}>Mapeathor: Simplifying the Specificationof Declarative Rules for KnowledgeGraph Construction</Title>
               </Col>
               <Col>
                 <Text type="secondary">ISWC 2020</Text>
@@ -99,13 +99,10 @@ const IndexPage = () => (
             </Row>
 
             <Paragraph className="text-justify">
-              In this paper we present Morph-CSV, a frameworkfor enhancing virtual knowledge graph access over a set of CSV files
-              by using a combination of CSVW annotations and RML mappings withFnO transformation functions. Exploiting these
-              inputs, the frameworkcreates an enriched RDB representation of the CSV files together withthe corresponding R2RML
-              mappings, enabling the use of existing querytranslation (SPARQL-to-SQL) techniques and tools. [...]
+              In this paper we present a user-friendly mapping-language-independent tool, Mapeathor, to declare transformation rulesbased  on  spreadsheets  and  translate  them  into  two  different  mappinglanguages with the purpose of easing the mappings creation process. [...]
             </Paragraph>
 
-            <Button type="primary" className="shadow">Continue Reading</Button>
+            <Button href="http://ceur-ws.org/Vol-2721/paper488.pdf" type="primary" className="shadow">Continue Reading</Button>
             </div>
           </div>
         </Col>
